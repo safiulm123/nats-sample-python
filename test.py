1d6e53cf3d05d64695b193d1956e930396062037
Safiul Hasan <safiulhasan@gmail.com>
2019-11-08 15:49:03 +0500

    Updated: Added Code to use JSON

--- test.py
+++ test.py
@@ -1,18 +1,29 @@
 import asyncio
+import json
 from nats.aio.client import Client as NATS
 from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 
 async def run(loop):
     nc = NATS()
-    data = {b"name" : b"safi", b"age" : b"39"}
+    data = {
+            "TimeStamp":"",
+            "Hostname":{
+                "IP":"",
+                "CPU":"",
+                "RAM":"",
+                "HDD":""
+            },
+            "Hostname":{
+                "IP":"",
+                "CPU":"",
+                "RAM":"",
+                "HDD":""
+            },
+}
 
     await nc.connect("localhost:4222", loop=loop)
-    await nc.publish("node-sub", data[b'name'])
-    await nc.publish("node-sub", b'World')
-    await nc.publish("node-sub", b'!!!!!')
+    await nc.publish("test", json.dumps(data).encode())
 
-    async def help_request(msg):
-        print(msg)
     # Terminate connection to NATS.
     await nc.close()
 
